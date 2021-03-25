449766270565c551f627e2aef8952843610a22c1
JoseRivera12 <josearmando1470@gmail.com>
2021-03-24 20:37:47 -0600

    readme actualizado y funcion error agregada

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Curso_MEVN
-Recopilacion de clases del curso 
+# Curso MERV
+
+## Clase 3
+
+### Recursos
+
+* [Historia de NodeJs](https://nodejs.dev/learn/a-brief-history-of-nodejs)
+* [Async y Callbacks Js](https://nodejs.dev/learn/javascript-asynchronous-programming-and-callbacks)
+* [Headers](https://www.iana.org/assignments/message-headers/message-headers.xhtml)
+* [Headers Mozilla](https://developer.mozilla.org/es/docs/Web/HTTP/Headers)
+* [Postman Echo](https://www.postman.com/postman/workspace/published-postman-templates/documentation/631643-f695cab7-6878-eb55-7943-ad88e1ccfd65?ctx=documentation)
--- src/server.js
+++ src/server.js
@@ -1,28 +1,69 @@
 const http = require("http");
-const https = require("https");
+const fs = require("fs");
+const path = require("path");
 
-http.get("http://postman-echo.com/get?foo1=bar1&foo2=bar2", (res) => res.pipe(process.stdout));
+const host = "localhost";
+const form = fs.readFileSync(path.join("public", "index.html"))
+const personas = fs.readFileSync(path.join("json", "personas.json"));
+const port = 3000;
 
-const payload = `{
-    "correo": "jose@gmail.com",
-    "nombre": "jose"
-}`;
+function requestListener(req, res){
+    if(req.method === "GET"){
+        get(res);
+        return;
+    }
+    if (req.method === "POST") {
+        post(req, res);
+        return;
+    }
+    error(405, res);
+}
 
-const opts = {
-    method: "POST",
-    hostname: "postman-echo.com",
-    path: "/post",
-    headers: {
-        "Content-Type": "application/json",
-        "Content-Length": Buffer.byteLength(payload),
-    },
-};
+function get(res){
+    res.writeHead(200, {
+        "Content-Type": "text/html",
+    });
 
-const req = http.request(opts, (res) => {
-    process.stdout.write("Status Code: " + res.statusCode + "\n");
-    process.stdout.write("Body: ");
-    res.pipe(process.stdout);
+    res.end(form);
+}
+
+
+function post(req, res) {
+    //if (req.headers["content-type"] !== "application/x-www-form-urlencoded") {
+    if (req.headers["content-type"] !== "application/json") {
+        error(415, res);
+        return;
+    }
+
+    let input = "";
+    req.on("data", (chunk) => {
+        console.log(chunk)
+        input += chunk.toString();
+    });
+
+    // req.on("end", () => {
+    //     console.log(input.toString());
+    //     res.end(http.STATUS_CODES[200]);
+    // });
+
+    req.on("end", () => {
+        const parsed = JSON.parse(input);
+        if (parsed.err) {
+            error(400, "Bad Request", res);
+            return;
+        }
+        console.log("Received data: ", parsed);
+        res.end('{"data": ' + input + "}");
+    });
+}
+
+function error(code, res) {
+    res.statusCode = code;
+    res.end(http.STATUS_CODES[code]);
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, ()=>{
+    console.log(`El servidor esta corriendo en http://${host}:${port}`);
 });
-
-req.on("error", (err) => console.error("Error: ", err));
-req.end(payload);
