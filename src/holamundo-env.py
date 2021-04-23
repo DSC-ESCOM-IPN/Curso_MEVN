41b14689ca1bddcb91f04899b618a6167162a99f
Carlos reyes <carloszrm90@gmail.com>
2021-04-23 18:51:51 -0500

    Hola mundo en node

--- src/holamundo-env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def hola_mundo(nombre):
-    print("Hola mundo", nombre)
-
-print(os.environ)
-nombre = os.environ["NAME"]
-hola_mundo(nombre)
--- src/holamundo.py
+++
@@ -1,3 +0,0 @@
-def hola_mundo():
-    print("Hola mundo")
-hola_mundo()
